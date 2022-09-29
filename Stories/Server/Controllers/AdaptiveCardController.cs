b58c977c59b2a3ed04e8989f54f3405a011fe5b8
lena.s <sanolenaa@gmail.com>
2022-09-29 07:11:20 -0700

    Add Adaptive Card for displaying user login form.
    AdaptiveCardsBlazor library is used to build platform-agnostic snippets of UI, authored in JSON, that apps and services can openly exchange.
    
    See https://adaptivecards.io/.

--- Stories/Server/Controllers/AdaptiveCardController.cs
+++ Stories/Server/Controllers/AdaptiveCardController.cs
@@ -4,16 +4,18 @@ using System.IO;
 
 [ApiController]
 [Route("[controller]")]
-public class StoryCardController : ControllerBase
+public class AdaptiveCardController : ControllerBase
 {
+
+
     [HttpGet]
-    public IActionResult GetStoryCardSchema()
+    public IActionResult GetCardSchema(string cardtype)
     {
         // return the StoryCardSchema.json file as StreamContent
         // reference: https://stackoverflow.com/questions/9541351/returning-binary-file-from-controller-in-asp-net-web-api?rq=1
 
         string schemaPath = Path.Combine(
-            Directory.GetCurrentDirectory(), "StoryCardSchema.json" // "WeatherCardSchema.json" //
+            Directory.GetCurrentDirectory(), $"{cardtype}CardSchema.json" // "WeatherCardSchema.json" //
         );
 
         FileStream stream = new FileStream(schemaPath, FileMode.Open, FileAccess.Read);
