11cf655292c6be3224a62ab4226ce0652aac6bb3
lena.s <sanolenaa@gmail.com>
2022-10-01 14:45:21 -0700

    Convert text to png, including line wrapping and rectangle. We do the conversion on the server-side, using the System.Drawing.Common .Net library. Since this library depends on the Windows GDI+ library, this limits the server to running on Windows OS.
    Added unit tests for SaveFileController.WrapTextWithGraphics function.

--- Stories/Server/Controllers/SaveFileController.cs
+++ Stories/Server/Controllers/SaveFileController.cs
@@ -9,6 +9,12 @@ using Microsoft.Extensions.Logging;
 using Stories.Shared;
 
 using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Drawing.Drawing2D;
+
+using System.Linq;
 
 [ApiController]
 [Route("[controller]")]
@@ -18,6 +24,8 @@ public class SaveFileController : ControllerBase
     private readonly ILogger<SaveFileController> logger;
 
     private static string UPLOAD_FOLDERNAME = "unsafe_uploads";
+
+    private static int IMAGE_WIDTH = 120;
 
     public SaveFileController(IWebHostEnvironment env,
         ILogger<SaveFileController> logger)
@@ -45,10 +53,13 @@ public class SaveFileController : ControllerBase
             {
                 string fullPath = Path.Combine(uploadFolder, Path.GetRandomFileName());
 
-                using (FileStream stream = new (fullPath, FileMode.Create))
+                using (FileStream stream = new(fullPath, FileMode.Create))
                 {
                     await stream.WriteAsync(Encoding.ASCII.GetBytes(filecontent));
                 }
+
+                SaveTextAsImage(filecontent, ImageFormat.Png);
+
                 return Ok(new object());
             }
             else
@@ -60,5 +71,117 @@ public class SaveFileController : ControllerBase
         {
             return StatusCode(500);
         }
+    }
+
+    public static void SaveTextAsImage(string text, ImageFormat f)
+    {
+        text = "The purpose of this challenge is to gather further evidence of the true abilities and qualifications of potential candidates for employment in Crawford Technologies software engineering division than would normally be possible via a simple interview. \nThe prospective employee is asked to take the enclosed project requirements and return to Crawford a functionally complete, working piece of software, for review by Crawford Technologies Management and Technical staff.Crawford’s staff will use this to evaluate the design decisions made by the prospective employee, as well as the craftsmanship and quality of the code and the project returned.This project will have a significant impact on the applicant selection process.Please take this opportunity to demonstrate for Crawford Technologies your skills in software engineering".Trim();
+
+
+        Bitmap bitmap = new (1, 1);
+
+        // Create a graphics object to measure the text's width and height.
+        Graphics g = Graphics.FromImage(bitmap);
+
+
+        // Set Background color
+        g.Clear(Color.White);
+        g.SmoothingMode = SmoothingMode.AntiAlias;
+        g.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+        // Create the Font object for the image text drawing.
+        Font font = new("Calibri Light", 20, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+
+        // Add text wrapping to the string.
+        string wrappedString = WrapTextWithGraphics( g, text + ' ', IMAGE_WIDTH, font);
+
+        // Measure string.
+        SizeF stringSize = g.MeasureString(wrappedString, font);
+
+        // Create the bmpImage again with the correct size for the text and font.
+        bitmap = new Bitmap(bitmap, Size.Round(stringSize) );
+        g = Graphics.FromImage(bitmap);
+
+        // Draw rectangle representing size of string.
+        g.DrawRectangle(new Pen(Color.Azure, 1), 0.0F, 0.0F, stringSize.Width, stringSize.Height);
+
+        // Draw string to screen.
+        g.DrawString(wrappedString, font, Brushes.Black, new PointF(0, 0));
+
+        g.Flush();
+        // ? g.Dispose();
+
+        string fileName = Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".png";
+
+        bitmap.Save( Path.Combine( Environment.CurrentDirectory, UPLOAD_FOLDERNAME, fileName), f);
+    }
+
+    /// <summary>
+    /// Applies word wrapping to a string, respecing word boundaries, if possible. The width is in pixels (For a font size 20, each character is about 7 pixels)
+    /// 
+    /// Example,
+    /// string result = SaveFileController.WrapTextWithGraphics(
+    ///     Graphics.FromImage(new Bitmap(1,1)), 
+    ///     "Hello this is a very long string, how long can it go? No one can say... okay, let's finish here. ", 
+    ///     300, 
+    ///     new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+    /// );
+    ///
+    /// result is "Hello this is a very long string, how \r\nlong can it go? No one can say... \r\nokay, let's finish here. "
+    /// </summary>
+    /// 
+    /// <param name="g">A graphics object, used for measuring line lengths</param>
+    /// <param name="original">The string to wrap. This string must end with a space (' ') or newline.</param>
+    /// <param name="width">The desired line width in pixels</param>
+    /// <param name="font">The font to be applied to the text</param>
+    /// <param name="wrappedLines">Used for recursive steps. The incremental list of lines of wrapped text.</param>
+    /// <param name="start">Used for recursive steps. The character position for the current iteration.</param>
+    /// 
+    /// <returns>The string with <see cref="Environment.NewLine"/> inserted.</returns>
+    
+    public static string WrapTextWithGraphics(in Graphics g, string original, in int width, in Font font, List<string> wrappedLines = null, int start = 0) 
+    {
+        if (wrappedLines is null) wrappedLines = new();                 // happens in the first iteration
+        if (original is null)     original = string.Empty;              // this might happen in the first iteration, if a null string is passed in on the first call to this function
+
+        //  _base case_:  empty string or reached the end
+
+        if ( start + 1 >= original.Length ) return string.Join(Environment.NewLine, wrappedLines);
+
+
+
+        // estimate the end position for the current line (for a size 20 font, divide by 7)
+        int end = start + (width / 7);                                  // overestinate the number of chars in the next line of text
+
+        end = end < original.Length ? end : original.Length - 1;        // in the last iteration, we need to adjust in case we are over the string length
+
+        // adjust for word boundaries
+        end = original.LastWordBoundaryBefore(end, start);
+
+        // find the end position of a space that fits the width
+        for
+        (
+            ; 
+            g.MeasureString(original.Substring(start, end - start + 1), font).Width > width;
+            end = original.LastWordBoundaryBefore( end - 1, start )
+        );
+
+        wrappedLines.Add( original.Substring(start, end - start + 1) );
+
+        return WrapTextWithGraphics(g, original, width, font, wrappedLines, end + 1);
+    }
+}
+
+public static class StringExtensions
+{
+    public static int LastWordBoundaryBefore(this string str, int end, int min)
+    {
+        int firstNewLine = str.Substring(min, end - min + 1).IndexOf(Environment.NewLine);
+        if (firstNewLine != -1) return min + firstNewLine + Environment.NewLine.Length - 1;                      // there is a newline in the string segment (adds one to account for two characters in windows (\r\n) if needed)
+
+        int lastSpace = str.Substring(min, end - min + 1).LastIndexOf(' ');
+        if (lastSpace != -1) return min + lastSpace;                          // return the position of the last space character (we don't handle other types of whitespace)
+
+        return end - 1;                                                 // there are no word breaks in the string segment, so reduce the range by one character (so that the width can be retested in the calling function)
     }
 }
---
+++ StoriesUnitTests/StringWrappingTests.cs
@@ -0,0 +1,218 @@
+using NUnit.Framework;
+using System.Drawing;
+using System;
+
+namespace StoriesUnitTests
+{
+    // N.B. These unit tests will succeed only on Windows because Environment.NewLine is "\r\n", and
+    //      because the System.Graphics library functions that are used are dependant on the Windows OS.
+
+    public class StringWrappingTests
+    {
+        Graphics g;
+        [SetUp]
+        public void Setup()
+        {
+
+            Bitmap bitmap = new Bitmap(1, 1);
+
+            // Create a graphics object to measure the text's width and height.
+            g = Graphics.FromImage(bitmap);
+        }
+
+        [Test]
+        public void TestTypical()
+        {
+            string actualResult =
+                SaveFileController.WrapTextWithGraphics(
+                    Graphics.FromImage(new Bitmap(1,1)), 
+                    "Hello this is a very long string, how long can it go? No one can say... okay, let's finish here. ", 
+                    300, 
+                    new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+                );
+
+            Assert.AreEqual("Hello this is a very long string, how \r\nlong can it go? No one can say... \r\nokay, let's finish here. ", actualResult);
+        }
+
+        [Test]
+        public void TestEmpty()
+        {
+            string actualResult =
+                SaveFileController.WrapTextWithGraphics(
+                    g,
+                    string.Empty,
+                    1000,
+                    new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+                );
+
+            Assert.AreEqual(string.Empty, actualResult);
+        }
+
+        [Test]
+        public void TestNull()
+        {
+            string actualResult = 
+                SaveFileController.WrapTextWithGraphics(
+                    g,
+                    null,
+                    1000,
+                    new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+                );
+
+            Assert.AreEqual(string.Empty, actualResult);
+        }
+
+        [Test]
+        public void TestParagraphs()
+        {
+            string actualResult =
+                SaveFileController.WrapTextWithGraphics(
+                    g,
+                    "The purpose of this challenge is to gather further evidence of the true abilities and qualifications of potential candidates for " +
+                    "employment in Crawford Technologies software engineering division than would normally be possible via a simple interview." +
+                    Environment.NewLine +
+                    "The prospective employee is asked to take the enclosed project requirements and return to Crawford a functionally complete, working " +
+                    "piece of software, for review by Crawford Technologies Management and Technical staff. Crawford’s staff will use this to evaluate " +
+                    "the design decisions made by the prospective employee, as well as the craftsmanship and quality of the code and the project returned.  " +
+                    "This project will have a significant impact on the applicant selection process. Please take this opportunity to demonstrate for " +
+                    "Crawford Technologies your skills in software engineering." +
+                    Environment.NewLine +
+                    "The Challenge" +
+                    Environment.NewLine +
+                    "John enjoys writing stories and sharing them with his friend Pat. Pat thinks his writing should be preserved for others to see and " +
+                    "enjoy. To this end, s / he has decided to build a website devoted to storing and preserving the stories for others to see. In addition, " +
+                    "Pat would like this website to return a copy of his writing embedded in a graphic (*.png) image. This embedded image should display the " +
+                    "stories enclosed in a rectangular box. (note, you do not need to write stories, you just need to show your ability with brief text " +
+                    "paragraphs.) Pat also wants to track how popular the different stories are, so she is going to log story views and downloads.S / he " +
+                    "will add a simple chart to each story’s page to show the data. ",
+                    1000,
+                    new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+                );
+
+            string expectedResult =
+                "The purpose of this challenge is to gather further evidence of the true abilities and qualifications of potential candidates \r\nfor " +
+                "employment in Crawford Technologies software engineering division than would normally be possible via a simple \r\ninterview.\r\n" +
+                Environment.NewLine +
+                "The prospective employee is asked to take the enclosed project requirements and return to Crawford a functionally \r\ncomplete, working " +
+                "piece of software, for review by Crawford Technologies Management and Technical staff. Crawford’s \r\nstaff will use this to evaluate " +
+                "the design decisions made by the prospective employee, as well as the craftsmanship and \r\nquality of the code and the project returned.  " +
+                "This project will have a significant impact on the applicant selection \r\nprocess. Please take this opportunity to demonstrate for " +
+                "Crawford Technologies your skills in software engineering.\r\n" +
+                Environment.NewLine +
+                "The Challenge\r\n" +
+                Environment.NewLine +
+                "John enjoys writing stories and sharing them with his friend Pat. Pat thinks his writing should be preserved for others to \r\nsee and " +
+                "enjoy. To this end, s / he has decided to build a website devoted to storing and preserving the stories for others \r\nto see. In addition, " +
+                "Pat would like this website to return a copy of his writing embedded in a graphic (*.png) image. This \r\nembedded image should display the " +
+                "stories enclosed in a rectangular box. (note, you do not need to write stories, you \r\njust need to show your ability with brief text " +
+                "paragraphs.) Pat also wants to track how popular the different stories are, \r\nso she is going to log story views and downloads.S / he " +
+                "will add a simple chart to each story’s page to show the data. ";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void TestParagraphsLongWord()
+        {
+            string actualResult =
+                SaveFileController.WrapTextWithGraphics(
+                    g,
+                    "The purpose of this challenge is_to_gather_further_evidence_of_the_true_abilities_and_qualifications_of potential candidates for " +
+                    "employment in Crawford Technologies software engineering division than would normally be possible via a simple interview." +
+                    Environment.NewLine +
+                    "The prospective employee is asked to take the enclosed project requirements and return to Crawford a functionally complete, working " +
+                    "piece of software, for review by Crawford Technologies Management and Technical staff. Crawford’s staff will use this to evaluate " +
+                    "the design decisions made by the prospective employee, as well_as_the_craftsmanship_and_quality_of_the_code_and_the_project_returned._" +
+                    "This_project_will_have_a_significant_impact_on the applicant selection process. Please take this opportunity to demonstrate for " +
+                    "Crawford Technologies your skills in software engineering." +
+                    Environment.NewLine +
+                    "The Challenge" +
+                    Environment.NewLine +
+                    "John enjoys writing stories and sharing them with his friend Pat. Pat thinks his writing should be preserved for others to see and " +
+                    "enjoy. To this end, s / he has decided to build a website devoted to storing and preserving the stories for others to see. In addition, " +
+                    "Pat would like this website to return_a_copy_of_his_writing_embedded_in_a_graphic_(*.png)_image._This embedded image should display the " +
+                    "stories enclosed in a rectangular box. ",
+                    300,
+                    new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+                );
+
+            string expectedResult =
+                "The purpose of this challenge \r\nis_to_gather_further_evidence_of\r\n_the_true_abilities_and_qualificat\r\nions_of potential candidates for \r\n" +
+                "employment in Crawford \r\nTechnologies software engineering \r\ndivision than would normally be \r\npossible via a simple interview.\r\n" +
+                Environment.NewLine +
+                "The prospective employee is asked \r\nto take the enclosed project \r\nrequirements and return to \r\nCrawford a functionally complete, \r\nworking " +
+                "piece of software, for \r\nreview by Crawford Technologies \r\nManagement and Technical staff. \r\nCrawford’s staff will use this to \r\nevaluate " +
+                "the design decisions \r\nmade by the prospective \r\nemployee, as \r\nwell_as_the_craftsmanship_and_\r\nquality_of_the_code_and_the_pr\r\noject_returned._" +
+                "This_project_will_\r\nhave_a_significant_impact_on the \r\napplicant selection process. Please \r\ntake this opportunity to \r\ndemonstrate for " +
+                "Crawford \r\nTechnologies your skills in software \r\nengineering.\r\n" +
+                Environment.NewLine +
+                "The Challenge\r\n" +
+                Environment.NewLine +
+                "John enjoys writing stories and \r\nsharing them with his friend Pat. \r\nPat thinks his writing should be \r\npreserved for others to see and \r\n" +
+                "enjoy. To this end, s / he has \r\ndecided to build a website devoted \r\nto storing and preserving the \r\nstories for others to see. In \r\naddition, " +
+                "Pat would like this \r\nwebsite to \r\nreturn_a_copy_of_his_writing_e\r\nmbedded_in_a_graphic_(*.png)_\r\nimage._This embedded image \r\nshould display the " +
+                "stories enclosed \r\nin a rectangular box. ";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void TestShortParagraphs()
+        {
+            string actualResult =
+                SaveFileController.WrapTextWithGraphics(
+                    g,
+                    "The Challenge" +
+                    Environment.NewLine +
+                    "John enjoys writing stories and sharing them with his friend Pat.Pat thinks his writing should be preserved for " +
+                    "others to see and enjoy. To this end, s / he has decided to build a website devoted to storing and preserving the " +
+                    "stories for others to see. In addition, Pat would like this website to return a copy of his writing embedded in a " +
+                    "graphic (*.png) image. This embedded image should display the stories enclosed in a rectangular box. (note, you do not " +
+                    "need to write stories, you just need to show your ability with brief text paragraphs.) Pat also wants to track how " +
+                    "popular the different stories are, so she is going to log story views and downloads. S / he will add a simple chart to " +
+                    "each story’s page to show the data." +
+                    Environment.NewLine +
+                    "These are Pat’s requirements." +
+                    Environment.NewLine +
+                    "1) Web site for stories" +
+                    Environment.NewLine +
+                    "2) Ability to submit and display stories" +
+                    Environment.NewLine +
+                    "3) Ability to download a graphic(*.png) with stories embedded" +
+                    Environment.NewLine +
+                    "4) log views and downloads" +
+                    Environment.NewLine +
+                    "5) Display a simple filtered chart of views and downloads over time ",
+                    1000,
+                    new Font("Calibri Light", 20, FontStyle.Regular, GraphicsUnit.Pixel)
+                );
+
+            string expectedResult =
+                "The Challenge\r\n" +
+                Environment.NewLine +
+                "John enjoys writing stories and sharing them with his friend Pat.Pat thinks his writing should be preserved for " +
+                "others to \r\nsee and enjoy. To this end, s / he has decided to build a website devoted to storing and preserving the " +
+                "stories for others \r\nto see. In addition, Pat would like this website to return a copy of his writing embedded in a " +
+                "graphic (*.png) image. This \r\nembedded image should display the stories enclosed in a rectangular box. (note, you do not " +
+                "need to write stories, you \r\njust need to show your ability with brief text paragraphs.) Pat also wants to track how " +
+                "popular the different stories are, \r\nso she is going to log story views and downloads. S / he will add a simple chart to " +
+                "each story’s page to show the data.\r\n" +
+                Environment.NewLine +
+                "These are Pat’s requirements.\r\n" +
+                Environment.NewLine +
+                "1) Web site for stories\r\n" +
+                Environment.NewLine +
+                "2) Ability to submit and display stories\r\n" +
+                Environment.NewLine +
+                "3) Ability to download a graphic(*.png) with stories embedded\r\n" +
+                Environment.NewLine +
+                "4) log views and downloads\r\n" +
+                Environment.NewLine +
+                "5) Display a simple filtered chart of views and downloads over time ";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
